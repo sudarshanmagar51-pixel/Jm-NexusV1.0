2e284658c54aa5a04fc99c9c2213b0bcf4f6045a
Sudarshan Magar <sudarshanmagar51@gmail.com>
2026-08-07 15:09:48 +0545

    Set up ReactDOM rendering for JMNexus component

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import JMNexus from "./App.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <JMNexus />
+  </React.StrictMode>
+);
